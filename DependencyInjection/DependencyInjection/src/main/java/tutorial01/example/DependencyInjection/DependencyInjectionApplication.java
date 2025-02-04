45132aa0dd3e6f0ebd341c16b80cc743eff31d0c
Isuru <isuruindrajith680@gmail.com>
2025-02-04 08:40:19 +0530

    AutoWiring

--- DependencyInjection/DependencyInjection/src/main/java/tutorial01/example/DependencyInjection/DependencyInjectionApplication.java
+++ DependencyInjection/DependencyInjection/src/main/java/tutorial01/example/DependencyInjection/DependencyInjectionApplication.java
@@ -2,13 +2,21 @@ package tutorial01.example.DependencyInjection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DependencyInjectionApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(DependencyInjectionApplication.class, args);
+//**type of the application container(IOC) is Application Context
+		ApplicationContext context = SpringApplication.run(DependencyInjectionApplication.class, args);
+//	2)	SpringApplication.run(DependencyInjectionApplication.class, args); here .run method is returning the objects of the application contex
+//		Creating the JVM IOC container for the Spring Boot to handle the objects
+
+		Dev obj = context.getBean(Dev.class);
+//	3) getBean is used to get the object of Dev class
+		obj.build();
 	}
-//	SpringApplication.run creates the IOC container(JVM object creation area)
+//	1) SpringApplication.run creates the IOC container(JVM object creation area)
 }
--- DependencyInjection/DependencyInjection/src/main/java/tutorial01/example/DependencyInjection/Dev.java
+++ DependencyInjection/DependencyInjection/src/main/java/tutorial01/example/DependencyInjection/Dev.java
@@ -1,8 +1,13 @@
 package tutorial01.example.DependencyInjection;
 
+import org.springframework.stereotype.Component;
+
+//by @Component spring will know the classes that it must manage.
+//That means spring will create the object in the IOC container for this class
+@Component
 public class Dev  {
 
     public void build(){
-        System.out.println("Building Spring Boot project")
+        System.out.println("Building Spring Boot project");
     }
 }
